b3753a885a7a80ef10a1ed09569eb0aa65ed43e0
rsato1007 <robertmsato@gmail.com>
2021-11-10 19:56:12 -0700

    adding all base templates

--- next_route/main_app/views.py
+++ next_route/main_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 # This is only temporary until I actually add something.
 from django.http import HttpResponse
 
 # Create your views here.
-class Splash(View):
-
-    def get(self, request):
-        return HttpResponse("This is your sanity check buddy")
+class Splash(TemplateView):
+    template_name = "splash.html"
