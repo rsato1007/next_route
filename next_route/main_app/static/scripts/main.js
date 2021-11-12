86175023e8d739534eeaf808978aa6f0d6891351
rsato1007 <robertmsato@gmail.com>
2021-11-12 13:11:37 -0700

    adding show and hide review form

--- next_route/main_app/static/scripts/main.js
+++ next_route/main_app/static/scripts/main.js
@@ -2,19 +2,38 @@ let userRouteEl = document.querySelector(".user_routes");
 let userReviewEl = document.querySelector(".user_reviews");
 const userReviewButtonEl = document.querySelector(".user_reviews_button");
 const userRouteButtonEl = document.querySelector(".user_routes_button");
+const writeReviewButtonEl = document.querySelector(".write-review-button");
 
-userRouteButtonEl.addEventListener("click", (e) => {
-    e.preventDefault();
-    userReviewEl.classList.remove("show");
-    userReviewEl.classList.add("no-show");
-    userRouteEl.classList.remove("no-show");
-    userRouteEl.classList.add("show");
-});
+const showElement = (domEl) => {
+    domEl.classList.add('show');
+    domEl.classList.remove("no-show");
+}
 
-userReviewButtonEl.addEventListener("click", (e) => {
-    e.preventDefault();
-    userRouteEl.classList.remove("show");
-    userRouteEl.classList.add("no-show");
-    userReviewEl.classList.remove("no-show");
-    userReviewEl.classList.add("show");
-});
+const hideElement = (domEl) => {
+    domEl.classList.add('show');
+    domEl.classList.remove("no-show");
+}
+
+if (userRouteButtonEl) {
+    userRouteButtonEl.addEventListener("click", (e) => {
+        e.preventDefault();
+        hideElement(userReviewEl);
+        showElement(userRouteEl);
+    });
+}
+
+if (userReviewButtonEl) {
+    userReviewButtonEl.addEventListener("click", (e) => {
+        e.preventDefault();
+        hideElement(userRouteEl);
+        showElement(userReviewEl);
+    });
+}
+
+if (writeReviewButtonEl) {
+    writeReviewButtonEl.addEventListener("click", (e) => {
+        e.preventDefault();
+        const routeReviewContainEL = document.querySelector(".route-review-form-container");
+        showElement(routeReviewContainEL);
+    });
+}
