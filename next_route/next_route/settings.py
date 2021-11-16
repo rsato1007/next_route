161ad2b3c16993ab8462180edc3f098560ea6215
rsato1007 <robertmsato@gmail.com>
2021-11-16 14:57:59 -0700

    using django-heroku

--- next_route/next_route/settings.py
+++ next_route/next_route/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
+
+django_heroku.settings(locals())
 
 env = environ.Env()
 environ.Env.read_env()
